cabc4191df044bfcd556531771b4368ed23f643c
Nouveau-Ubuntu-Lab <409986157@qq.com>
2024-03-01 18:34:17 +0800

    2024/3/1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main() {
-    cv::Mat image(500, 500, CV_8UC3, cv::Scalar(255, 255, 255));
-
-    if (image.empty()) {
-        std::cerr << "Error: Could not create the image." << std::endl;
-        return -1;
-    }
-
-    cv::imshow("Blank Image", image);
-    cv::waitKey(0);
+int main()
+{
 
     return 0;
 }
